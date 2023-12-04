037dc0dcaf98dc9455024678bed31d6b380d0253
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-04 23:44:42 +0545

    php backend

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+$server="localhost";
+$username="root";
+$password="";
+$dbname="login";
+
+$connection=mysqli_connect($server,$username,$password,$dbname);
+
+if(!$connection){
+    die("Connection failed to database" . mysqli_connect_error());
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $sql="INSERT INTO `login` (`email`, `password`) VALUES ('$email', '$password');";
+
+    if(mysqli_query($connection,$sql)){
+        echo "Data inserted successfully!";
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+    }
+}
+
+mysqli_close($connection);
+?>
+
+
