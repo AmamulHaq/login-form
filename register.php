b8da6cdd80d666d251c58e5f038627a427317a0e
GOD FATHER <132338103+AmamulHaq@users.noreply.github.com>
2023-12-26 20:53:49 +0545

    inserted data in mySQL

--- register.php
+++ register.php
@@ -1,38 +1,3 @@
-<?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "login";
-
-$connection = mysqli_connect($server, $username, $password, $dbname);
-
-if (!$connection) {
-    die("Connection failed to database: " . mysqli_connect_error());
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $repass = $_POST['repass'];
-    if ($password != $repass) {
-        die("Re confirm...Passwords do not match");
-    }
-
-    
-    
-    $sql = "INSERT INTO `login`.`login` (`name`, `email`, `password`, `repass`) VALUES ('$name', '$email', '$password', '$repass');";
-
-    if (mysqli_query($connection, $sql)) {
-        echo ("Data inserted successfully!");
-        header("Location: login.html");
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
-    }
-}
-
-mysqli_close($connection);
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -77,3 +42,40 @@ mysqli_close($connection);
 </body>
 
 </html>
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login";
+
+$connection = mysqli_connect($server, $username, $password, $dbname);
+
+if (!$connection) {
+    die("Connection failed to database: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $repass = $_POST['repass'];
+    if ($password != $repass) {
+        die("Re confirm...Passwords do not match");
+    }
+
+    
+    
+    $sql = "INSERT INTO `login`.`login` (`name`, `email`, `password`, `repass`) VALUES ('$name', '$email', '$password', '$repass');";
+
+    if (mysqli_query($connection, $sql)) {
+        echo ("Data inserted successfully!");
+        header("Location: index.html");
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+    }
+}
+
+mysqli_close($connection);
+?>
+
+
